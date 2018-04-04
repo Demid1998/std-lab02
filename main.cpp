c127fb5c3c11b3892fb9d1b0791386034d2b3d0d
Demid Chernykh <demid1998@yandex.ru>
2018-04-05 01:29:57 +0300

    добавил умножение

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 //программа является учебной
 int main() {
-    int a,b;
-    cout<<"Enter A and B:" ;
-    cin>>a>>b;
-    cout<<"A+B="<<a+b<<'\n';
-    cout<<"A*2="<<a*2<<'\n';
-    cout<<"A-B="<<a-b<<'\n';
+    int a, b;
+    cout << "Enter A and B:";
+    cin >> a >> b;
+    cout << "A+B=" << a + b << '\n';
+    cout << "A*2=" << a * 2 << '\n';
+    cout << "A-B=" << a - b << '\n';
+    cout << "A*B=" << a * b << '\n';
 }
